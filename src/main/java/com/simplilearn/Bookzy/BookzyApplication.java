66710d4f7469809f1d7685e0e7013721d8f07ccd
jeffmyers81 <jeffmyers81@gmail.com>
2021-02-06 21:13:35 +0000

    Updated app and tests

--- src/main/java/com/simplilearn/Bookzy/BookzyApplication.java
+++ src/main/java/com/simplilearn/Bookzy/BookzyApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookzyApplication {
-
+    	public static double getRandomNumber(){
+   		 double min=1000;
+	   	 double max=9999;
+   		 double x = (int)(Math.random()*((max-min)+1))+min;
+   		 return x;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BookzyApplication.class, args);
 	}
--- src/test/java/com/simplilearn/Bookzy/BookzyApplicationTests.java
+++ src/test/java/com/simplilearn/Bookzy/BookzyApplicationTests.java
@@ -2,6 +2,10 @@ package com.simplilearn.Bookzy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BookzyApplicationTests {
@@ -9,5 +13,12 @@ class BookzyApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	private BookzyApplication ticketer = new BookzyApplication();
+	@Test	 
+	public void testRandomNumber() {     	 
+   		assertTrue(ticketer.getRandomTicketNumber()>1000&&ticketer.getRandomTicketNumber()<9999);	 
+	}	
+
 
 }
